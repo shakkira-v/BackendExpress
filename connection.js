08f44fd838285dffb1942b5087be3874f43c2edd
shakkira-v <shakkira111@gmail.com>
2026-01-19 14:01:16 +0530

    second commit

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export default async function collection() {
     const db = await mongoose.connect(
-      `mongodb+srv://shakkiraventhodi_db_user:Alliswell123@cluster0.7h2xfia.mongodb.net/BACKENDEXPRESS`,
+      `mongodb+srv://shakkira111_db_user:SsQvl7N20f2oISOi@cluster0.pgtelan.mongodb.net/BACKENDEXPRESS`,
     );
     console.log("Database created");
 
